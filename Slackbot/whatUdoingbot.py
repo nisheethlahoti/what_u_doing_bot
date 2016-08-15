e7bf72df80eb8da3b0e23dffd6df32071335c1ac
Nisheeth Lahoti <nisheeth@Nisheeths-MacBook-Pro.local>
2016-08-16 02:10:20 +0530

    Debugged and ported to Python 3

--- Slackbot/whatUdoingbot.py
+++ Slackbot/whatUdoingbot.py
@@ -18,6 +18,7 @@ REQUEST_FOR_UPDATE = "Hey, just checking up. Can you let me know what have you b
 INVALID_INPUT = "Not sure what you mean. Type help to get possible commands"
 LOGIN_REQUIRED = "You have to be logged in to use this command!"
 LOGOUT_REQUIRED = "Can't do this when already logged in!"
+NO_ACCEPT_ARGUMENTS = "Don't understand this command if followed by further text :/"
 
 # instantiate Slack & Twilio clients
 slack_client = SlackClient("")  # TODO: Should have the slack token
@@ -26,11 +27,11 @@ slack_client = SlackClient("")  # TODO: Should have the slack token
 class User:
     # TODO - Add for options for break and end of day
     # TODO - Implement help function
-    def __init__(self, id, name):
-        self.id = id
+    def __init__(self, user_id, name):
+        self.id = user_id
         self.name = name
         self._logged_in = False
-        self._login_time = datetime()
+        self._login_time = None
         self._timer = None
 
     def _post_message(self, message):
@@ -44,49 +45,51 @@ class User:
                     func(self, *args)
                 else:
                     self._post_message(LOGIN_REQUIRED if desired else LOGOUT_REQUIRED)
-            with_args.func_dict = func.func_dict
+            with_args.__dict__ = func.__dict__
             return with_args
         return with_fn
 
     def _command(func):
-        def one_argument_fn(self, x):
-            if x:
-                self.post_message("This command does not take arguments")
-            else:
-                func(self)
-        if inspect.getargspec(func).args.len() == 1:
-            func = one_argument_fn
-        func.func_dict['command'] = True
-        return func
+        num_params = len(inspect.signature(func).parameters)-1
+
+        def one_argument_fn(self, *args):  # args and num_params can both only be 0 or 1
+            if len(args) == num_params:
+                func(self, *args)
+            elif args:
+                self._post_message(NO_ACCEPT_ARGUMENTS)
+            elif num_params:
+                func(self, "")
+        one_argument_fn.__dict__['command'] = True
+        return one_argument_fn
 
     def _timely_followup(self):
         self._post_message(REQUEST_FOR_UPDATE)
         self._timer = threading.Timer(FOLLOWUP_TIME, self._timely_followup)
         self._timer.start()
 
-    @_command
     @_assert_login_status(False)
+    @_command
     def login(self):
         self._logged_in = True
         self._post_message(MORNING_MESSAGE)
         self._login_time = datetime.now()
         self._timer = threading.Timer(FOLLOWUP_TIME, self._timely_followup)
         self._timer.start()
-        print "Login time of " + self.name + " is " + str(self._login_time)
+        print("Login time of " + self.name + " is " + str(self._login_time))
 
-    @_command
     @_assert_login_status(True)
+    @_command
     def update(self, content):
-        print "Work Update from " + self.name + " is:" + content
+        print("Work Update from " + self.name + " is:" + content)
 
-    @_command
     @_assert_login_status(True)
+    @_command
     def logout(self):
         logout_time = datetime.now()
-        print "Logout time of " + self.name + " is " + str(logout_time)
+        print("Logout time of " + self.name + " is " + str(logout_time))
         self._logged_in = False
-        if self.timer:
-            self.timer.cancel()
+        if self._timer:
+            self._timer.cancel()
 
     def handle_command(self, user_input):
         """
@@ -96,13 +99,13 @@ class User:
         """
         tokens = user_input.split(None, 1)
         if not tokens:
-            self.post_message("Don't know what to do with empty command :(")
+            self._post_message("Don't know what to do with empty command :(")
         else:
             func = getattr(self, tokens[0], None)
-            if callable(func) and func.func_dict['command']:
+            if callable(func) and func.__dict__['command']:
                 func(*tokens[1:])
             else:
-                self.post_message(INVALID_INPUT)
+                self._post_message(INVALID_INPUT)
 
 
 users = {User("<uid>", "@<username>"),
@@ -115,11 +118,6 @@ def find_user(user_id):
 
 
 def parse_slack_output(slack_rtm_output):
-    """
-        The Slack Real Time Messaging API is an events firehose.
-        this parsing function returns None unless a message is
-        directed at the Bot, based on its ID.
-    """
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
@@ -136,6 +134,7 @@ if __name__ == "__main__":
         while True:
             command, channel = parse_slack_output(slack_client.rtm_read())
             if command and channel:
+                print("Received message at " + channel)
                 find_user(channel).handle_command(command)
             time.sleep(1)  # 1 second delay between reading from firehose
     else:
